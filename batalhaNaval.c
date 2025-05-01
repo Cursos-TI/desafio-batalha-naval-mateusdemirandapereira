466dcf64912709212911c57f9952f421d08a47e2
mateusdemirandapereira <terrymafa@gmail.com>
2025-05-01 08:49:48 -0300

    adicionando os navios no tabuleiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -15,6 +15,57 @@ void preecherMatrix(int * matrix, int linha,int coluna ) {
 
 }
 
+
+void adicionarNaviosVertical(int * matrix, int linha, int coluna) {
+	int posicao = 3;
+
+	for (int i = 0; i < linha; i++) {
+
+		for (int j = 0; j < coluna; j++) {
+
+			if (i >= 3) {
+				if (j == 4 && posicao > 0) {
+
+					*(matrix + i * coluna + j) = 3;
+					posicao--;
+				}
+
+			}
+
+		}
+
+
+	}
+
+
+}
+
+void adicionarNaviosHorizontal(int * matrix, int linha, int coluna) {
+	int posicao = 3;
+
+	for (int i = 0; i < linha; i++) {
+
+		for (int j = 0; j < coluna; j++) {
+
+			if (i == 1) {
+				if (j >= 3 && posicao > 0) {
+
+					*(matrix + i * coluna + j) = 3;
+					posicao--;
+				}
+
+			}
+
+		}
+
+
+	}
+
+
+}
+
+
+
 void mostrarMatrix(int * matrix, int linha ,int coluna ) {
 
 
@@ -40,6 +91,11 @@ int main() {
 	const int coluna = sizeof(matrix[0]) / sizeof(matrix[0][0]);
 
 	preecherMatrix((int *) matrix, linha, coluna);
+
+	adicionarNaviosVertical((int *)matrix, linha, coluna);
+
+	adicionarNaviosHorizontal((int *)matrix, linha, coluna);
+
 	mostrarMatrix((int *) matrix, linha, coluna);
 	return 0;
 
